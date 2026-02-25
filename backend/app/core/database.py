e4d133a9a2a9b334ad738ca2fc05e162a1751030
Mohanned Mahmoud <155165101+Mohanned-Mahmoud@users.noreply.github.com>
2026-02-25 23:48:02 +0200

    Update database.py

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -2,20 +2,27 @@ import os
 from sqlmodel import SQLModel, create_engine, Session
 from dotenv import load_dotenv
 
-# السطر ده هو اللي بيدور على ملف .env ويقرا اللي جواه
 load_dotenv()
 
-# هنا بيسحب اللينك من الـ env، ولو ملقاهوش هيضرب إيرور عشان ينبهك
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL is missing from .env file!")
 
-engine = create_engine(DATABASE_URL)
+# التعديل السحري هنا
+engine = create_engine(
+    DATABASE_URL,
+    # 1. بنجبره يستخدم SSL عشان Neon بيشترط ده للاتصال السحابي
+    connect_args={"sslmode": "require"}, 
+    # 2. بيتأكد إن الاتصال "صاحي" قبل ما يبعت أي Query (بيحل مشكلة الـ closed unexpectedly)
+    pool_pre_ping=True, 
+    # 3. بيجدد الاتصال كل 5 دقائق عشان Vercel ما يقطعوش
+    pool_recycle=300 
+)
 
 def get_session():
     with Session(engine) as session:
         yield session
 
 def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
+    SQLModel.metadata.create_all(engine)    
